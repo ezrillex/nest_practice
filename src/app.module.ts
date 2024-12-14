fef620764f82b5355efba1570817b220e8950d64
Ezra Abarca <39611157+ezrillex@users.noreply.github.com>
2024-12-13 19:43:46 -0600

    snapshot of files after some practice

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,19 @@ import { LoggerMiddleware } from './logger/logger.middleware';
 import { GamesService } from './gaming/games-service/games.service';
 import { ConsoleMediaService } from './gaming/console-media/console-media.service';
 import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
     GamingModule,
     // Cahe timings: first request/cold start 31 ms. next requests around 5 to 7 ms.
-    ConfigModule.forRoot({ cache: true, isGlobal: true }),
+    ConfigModule.forRoot({
+      cache: true, // added this after snapshot. My push failed so it didn't show :v
+      isGlobal: true,
+      validationSchema: Joi.object({
+        SECRET: Joi.string().required().valid('SAUCE'),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, GamesService, ConsoleMediaService],
